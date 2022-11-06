61de2b5461465bd43b1b9464149c5d9088f786ce
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-11-05 22:38:40 -0400

    styled homepage

--- client/src/home/Homepage.jsx
+++ client/src/home/Homepage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    
+      <div className="bg-dark text-light">
+        <Container>
+          <Row className="pt-5" xs={1} md={2}>
+            <Col>
+              <h1>Budget App</h1>
+              <br/>
+              <h4>Improve your spending habits with 5 minutes a day</h4>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
   )
 }
 
