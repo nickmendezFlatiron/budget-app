a26c7a68e380c4ca945fef530b235fbd035acf1c
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-11-05 23:31:08 -0400

    added image to homepage

--- client/src/home/Homepage.jsx
+++ client/src/home/Homepage.jsx
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import homepageImage from "../assets/homepage-image.png";
+
 const Homepage = () => {
   return (
     
@@ -13,6 +15,9 @@ const Homepage = () => {
               <h1>Budget App</h1>
               <br/>
               <h4>Improve your spending habits with 5 minutes a day</h4>
+            </Col>
+            <Col>
+              <img src={homepageImage} alt="image" className='homepage-image'/>
             </Col>
           </Row>
         </Container>
