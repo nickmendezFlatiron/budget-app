7d8358056681090a70ad9d8732ea1701afcb07b1
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-12-01 08:53:54 -0500

    conditionally display save button in account component

--- client/src/account/Account.jsx
+++ client/src/account/Account.jsx
@@ -2,21 +2,25 @@ import React, {useState} from 'react'
 
 import Container from 'react-bootstrap/Container'
 import ToggleButton from 'react-bootstrap/ToggleButton'
+import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 const Account = ({user}) => {
-  const [isDisabled, toggleDisabled] = useState(true)
+  const [isDisabled, toggleDisabled] = useState(false)
 
-
+  const saveButton = <Button size='sm'>Update</Button>
+  const pencil = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                  </svg>
   return (
-    <div className="bg-dark text-white vh-100">
-      <Container >
+    <div className="bg-dark text-white ">
+      <Container className="py-4 ">
         <Row className="text-primary pt-3">
           <h1>{!isDisabled ? "Edit" : null } {user.username}'s Profile</h1>
         </Row>
-        <Row xs={1} md={2} className="bg-secondary bg-opacity-25 rounded glow p-5 ">
+        <Row xs={1} md={2} className="bg-secondary bg-opacity-25 rounded glow p-5">
           <Col className="text-center">
             <Image className='glow rounded-circle' fluid src={user.avatar} alt="user avatar image" />
           </Col>
@@ -38,16 +42,20 @@ const Account = ({user}) => {
                 <h6>{user.linkedAccounts[0].institution}</h6>
               </Col>
             </Col>
-            <ToggleButton
-            className='mt-3'
-            id="toggle-check"
-            type="checkbox"
-            variant='outline-primary'
-            checked={!isDisabled}
-            onChange={(e) => toggleDisabled(!e.currentTarget.checked)}
-            >
-              Edit Profile
-            </ToggleButton>
+            <div className='mt-3'>
+              <ToggleButton     
+              className="me-3 "         
+              id="toggle-check"
+              size='sm'
+              type="checkbox"
+              variant='outline-primary'
+              checked={!isDisabled}
+              onChange={(e) => toggleDisabled(!e.currentTarget.checked)}
+              >
+              {pencil} Edit Profile
+              </ToggleButton>
+              {!isDisabled ? saveButton : null}
+            </div>
           </Col>
         </Row>
       </Container>
