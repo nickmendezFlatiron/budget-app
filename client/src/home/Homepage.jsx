75b8241a2f9e771a757d1ce3c877853c0d681868
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-12-08 12:05:30 -0500

    imported usenavigate to homepage component

--- client/src/home/Homepage.jsx
+++ client/src/home/Homepage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom'
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,7 +11,7 @@ import homepageImage from "../assets/homepage-image.png";
 import textLogo from "../assets/text-logo.png";
 
 const Homepage = ({isAuthenticated}) => {
-
+  const navigate = useNavigate()
   return (
     
       <div className="bg-dark text-light pb-5">
@@ -21,10 +23,12 @@ const Homepage = ({isAuthenticated}) => {
               <br/>
               <h3>Create better spending habits with 5 minutes a day.</h3>
               <br/>
-        { !isAuthenticated && <Col className="d-flex gap-3">
-                <Button variant="warning" sz="lg" className="text-dark fw-bold shadow">CREATE AN ACCOUNT</Button>
-                <Button variant="light" sz="lg" className="text-dark fw-bold">LOGIN</Button>
-              </Col>}
+              { !isAuthenticated && 
+                <Col className="d-flex gap-3">
+                  <Button onClick={() => navigate('/login')} variant="warning" sz="lg" className="text-dark fw-bold shadow">CREATE AN ACCOUNT</Button>
+                  <Button onClick={() => navigate('/login')} variant="light" sz="lg" className="text-dark fw-bold">LOGIN</Button>
+                </Col>
+              }
             </Col>
             <Col >
               <img src={homepageImage} alt="image" className='homepage-image'/>
